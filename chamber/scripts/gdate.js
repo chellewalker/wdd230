ad9ee6f621a172ca2ce6fccfcc02e9d76c90916d
chellewalker <war19006@byui.edu>
2022-10-05 19:14:12 -0600

    base start

---
+++ chamber/scripts/gdate.js
@@ -0,0 +1,14 @@
+function gdate() {
+    var currentTime = new Date();
+
+    var year = currentTime.getFullYear();
+
+    var monthn = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+
+    var day = currentTime.getDay();
+
+    var dayn = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    document.write(dayn[currentTime.getDay]+', '+day+' '+monthn[currentTime.getMonth]+''+year);
+}
