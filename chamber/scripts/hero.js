fc25c27e1d859249b7a0cf2bf16926ed7b24d304
chellewalker <war19006@byui.edu>
2022-10-16 17:51:38 -0600

    started overlay

--- chamber/scripts/hero.js
+++ chamber/scripts/hero.js
@@ -3,17 +3,23 @@ function hero() {
 
     // Small
     if (width <= 840) {
-    document.write('<img src="images/golf.webp" alt="golfing-image" class="center">')
-    document.write('small')
+        document.write('<div class="container">')
+        document.write('<img src="images/golf.webp" alt="golfing-image" class="center">')
+        document.write('<div class="bottom-right">small</div>')
+        document.write('</div>')
     }
     // Large
     else if (width >= 1280) {
+        document.write('<div class="container">')
         document.write('<img src="images/golf.webp" alt="golfing-image" class="center">')
-        document.write('large')
+        document.write('<div class="bottom-right">large</div>')
+        document.write('</div>')
      }
     // Medium
     else {
+        document.write('<div class="container">')
         document.write('<img src="images/golf.webp" alt="golfing-image" class="center">')
-        document.write('medium')
+        document.write('<div class="bottom-right">medium</div>')
+        document.write('</div>')
         }
 }
