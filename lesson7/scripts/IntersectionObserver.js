5fb3de0022594f07e7682fc80c7991f4c6f6230c
chellewalker <war19006@byui.edu>
2022-10-27 14:05:01 -0600

    minor adjustment

--- lesson7/scripts/IntersectionObserver.js
+++ lesson7/scripts/IntersectionObserver.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var lazyloadImages = document.querySelectorAll("img.lazy");    
-    var lazyloadThrottleTimeout;
+    var lazyImages = document.querySelectorAll("img.lazy");    
+    var lazyTimeout;
     
     function lazyload () {
-      if(lazyloadThrottleTimeout) {
-        clearTimeout(lazyloadThrottleTimeout);
+      if(lazyTimeout) {
+        clearTimeout(lazyTimeout);
       }    
       
-      lazyloadThrottleTimeout = setTimeout(function() {
-          var scrollTop = window.pageYOffset;
-          lazyloadImages.forEach(function(img) {
-              if(img.offsetTop < (window.innerHeight + scrollTop)) {
+      lazyTimeout = setTimeout(function() {
+          var scroll = window.pageYOffset;
+          lazyImages.forEach(function(img) {
+              if(img.offsetTop < (window.innerHeight + scroll)) {
                 img.src = img.dataset.src;
                 img.classList.remove('lazy');
               }
           });
-          if(lazyloadImages.length == 0) { 
+          if(lazyImages.length == 0) { 
             document.removeEventListener("scroll", lazyload);
             window.removeEventListener("resize", lazyload);
             window.removeEventListener("orientationChange", lazyload);
