274ee43ec8c88861a40c68139353c4577ea86bff
chellewalker <war19006@byui.edu>
2022-12-09 17:19:45 -0700

    good enough

--- bountiful/scripts/hero.js
+++ bountiful/scripts/hero.js
@@ -5,21 +5,21 @@ function hero() {
     if (width <= 840) {
         document.write('<div class="container">')
         document.write('<source media="min-width:800px" srcset="images/golf.webp" alt="golfing-image" class="center">')
-        document.write('<img src="images/golf.webp" alt="golfing-image" class="center">')
+        document.write('<img src="images/smoothie.webp" alt="smoothie-image" class="center">')
         document.write('</div>')
     }
     // Large
     else if (width >= 1280) {
         document.write('<div class="container">')
         document.write('<source media="min-width:1200px" srcset="images/golf.webp" alt="golfing-image" class="center">')
-        document.write('<img src="images/golf.webp" alt="golfing-image" class="center">')
+        document.write('<img src="images/smoothie.webp" alt="smoothie-image" class="center">')
         document.write('</div>')
      }
     // Medium
     else {
         document.write('<div class="container">')
         document.write('<source media="min-width:1000px" srcset="images/golf.webp" alt="golfing-image" class="center">')
-        document.write('<img src="images/golf.webp" alt="golfing-image" class="center">')
+        document.write('<img src="images/smoothie.webp" alt="smoothie-image" class="center">')
         document.write('</div>')
         }
 }
